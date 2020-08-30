f4311ced4ecb869516b3b0899ea605f6ef962964
Zbyszard <florcmat@gmail.com>
2020-08-30 20:05:51 +0200

    calling from matlab

--- src/python/stripmatlabcomments.py
+++ src/python/stripmatlabcomments.py
@@ -1,12 +1,11 @@
 import sys
 import argparse
 import re
-from timeit import default_timer as timer
+from enum import Enum
 
 
 def main(arg_obj):
     stripper = MatlabCommentStripper(arg_obj.mark)
-    start = timer()
     if arg_obj.string:
         istr_lines = arg_obj.string.split('\n')
     else:
@@ -16,36 +15,47 @@ def main(arg_obj):
         stripper.write_to(arg_obj.ofile, ostr)
     else:
         print(ostr)
-    stop = timer()
-    print(f"Execution time: {stop - start}")
 
 
 line_regex_str = r"""
-    (?P<code>
-        ^
-        (?:
-            (?:
-                [\]\)}\w.]
-                    '+
-                |
-                [^'\"%\n]
-            )+
-            |
-            (?P<q>'|\")
-                (?:
-                    (?:(?P=q){2})*
-                    (?:(?!(?P=q)|\n).)*
-                )*
-            (?P=q)?
-        )*
-    )
-    (?P<comment>[^\n]*\n?)
+    (?P<code>                                   (?# code group)
+        ^                                       (?# line beginning)
+        (?:                                 
+            (?:                                 
+                [\]\)}\w.]                      (?# any char than can be followed by)
+                    '+                          (?# one or more transpose operators)
+                |                               (?# or)
+                [^'\"%\n]                       (?# any char excluding quotes)
+            )+                              
+            | 
+            (?:                                 (?# string group)
+                (?P<q>'|\")                     (?# match starting quote and remember it)
+                    (?:                         (?# string content group)
+                        (?:(?P=q){2})*          (?# try to match many embedded starting quotes)
+                        (?:(?!(?P=q)|\n).)*     (?# or any other chars except new lines)
+                    )*                          
+                (?P=q)?                         (?# try to match ending quote)
+                                                (?# if string is not ended with proper quote)
+                                                (?# then rest of the line is treated as a string)
+            )                                   (?# end string group)                                   
+        )*                                      
+    )                                           (?# end code group)
+    (?P<comment>[^\n]*\n?$)
     """
-line_regex_str = re.sub(r"\s*", '', line_regex_str)
+# delete white spaces and comments from regex
+line_regex_str = re.sub(r"(\s*)|(\(\?\#.*\))", '',
+                        line_regex_str)
 line_regex = re.compile(line_regex_str)
 
+# match multiline comment start or end and nothing more except white spaces
 start_multiline_regex = re.compile(r"^\s*%{\s*\n?$")
 end_multiline_regex = re.compile(r"^\s*%}\s*\n?$")
+
+
+class MultilineState(Enum):
+    CODE = 0
+    COMMENT = 1
+    DELETE = 2
 
 
 class MatlabCommentStripper:
@@ -63,43 +73,60 @@ class MatlabCommentStripper:
         return ''.join(istr_lines)
 
     def strip_multiline_comments(self, istr_lines: list) -> (list, list):
-        """returns list of lines and a list of multiline comment line numbers"""
+        """returns list of lines and a list of numbers of lines being multiline comments"""
+        # output
         out = []
-        out_line_num = -1
+        # numbers of every line being a multiline comment
         comment_line_nums = []
-        content_stack = ["code"] 
+        # number of current output line
+        out_line_num = -1
+        # top element tells if current line contains
+        # code, comment or comment that should be deleted
+        content_stack = [MultilineState.CODE]
+
         line_num = -1
         max_line = len(istr_lines) - 1
         while line_num < max_line:
             line_num += 1
-            if content_stack[-1] != "delete":
+            if content_stack[-1] != MultilineState.DELETE:
                 out_line_num += 1
             line = istr_lines[line_num]
             next_line = istr_lines[line_num +
                                    1] if line_num + 1 < max_line else None
+            # if multiline comment started
             if self.start_multiline_regex.match(line):
-                if not self.deletion_mark or \
-                    next_line and re.sub(r"\s*", '', next_line) == self.deletion_mark or \
-                        content_stack[-1] == "delete":
-                    content_stack.append("delete")
+                # if already deleting comments or this comment should be deleted
+                if content_stack[-1] == MultilineState.DELETE or not self.deletion_mark or \
+                        next_line and re.sub(r"\s*", '', next_line) == self.deletion_mark:
+                    content_stack.append(MultilineState.DELETE)
                 else:
-                    content_stack.append("comment")
+                    content_stack.append(MultilineState.COMMENT)
+            # if multiline comment ended
             elif self.end_multiline_regex.match(line):
                 prev_state = content_stack.pop()
-                if prev_state == "comment":
-                    comment_line_nums.append(out_line_num)
-                elif prev_state == "delete":
+                # protection against redundant "%}"
+                if len(content_stack) == 0:
+                    content_stack.append(MultilineState.CODE)
+                if prev_state == MultilineState.DELETE:
                     continue
-            if content_stack[-1] == "delete":
+                elif prev_state == MultilineState.COMMENT:
+                    comment_line_nums.append(out_line_num)
+                    out.append(line)
+                    continue
+            if content_stack[-1] == MultilineState.DELETE:
                 continue
-            elif content_stack[-1] == "comment":
+            elif content_stack[-1] == MultilineState.COMMENT:
                 comment_line_nums.append(out_line_num)
             out.append(line)
         return out, comment_line_nums
 
     def strip_line_comments(self, istr_lines: list, line_nums_to_omit: list) -> list:
+
         out = []
-        deletion_mark = '%' + (self.deletion_mark or "")
+        if self.deletion_mark:
+            deletion_mark = '%' + self.deletion_mark + ' '
+        else:
+            deletion_mark = '%'
         for i, line in enumerate(istr_lines):
             if i in line_nums_to_omit or line.isspace():
                 out.append(line)
@@ -107,7 +134,7 @@ class MatlabCommentStripper:
             match = self.line_regex.match(line)
             code = match.group("code")
             comment = match.group("comment")
-            # if comment starts with deletion mark, delete comment
+            # if comment starts with deletion mark, delete it
             if comment[:len(deletion_mark)] == deletion_mark:
                 # if comment is deleted and there is no code, don't append line
                 if re.match(r"^\s*$", code):
@@ -123,7 +150,7 @@ class MatlabCommentStripper:
                 return file.readlines()
         except OSError as e:
             print(e)
-            exit(1)
+            exit(e.errno)
 
     @staticmethod
     def write_to(file_path: str, content) -> None:
@@ -132,9 +159,10 @@ class MatlabCommentStripper:
                 file.write(content)
         except OSError as e:
             print(e)
-            exit(1)
+            exit(e.errno)
 
 
+# delete variables from file scope
 del line_regex_str, line_regex, start_multiline_regex, end_multiline_regex
 
 
