99740c2df3a134a60dee36a34b69842026070ec7
florcmat <florcmat@gmail.com>
2020-10-21 18:48:53 +0200

    Updated tests

--- tests/testfile.m
+++ tests/testfile.m
@@ -1,35 +1,35 @@
-% default comment
-%$ marked comment
-args = {inputFile, outputFile, deletionMark}; %$1 comment
+% default comment without code - this line will be deleted completely
+%$ marked comment without code - this line will be deleted completely
+args = {inputFile, outputFile, deletionMark}; %$1 comment after code - only comment will be deleted
 for ii = 1:nargin
     if ~isstring(args{ii}) && ~ischar(args{ii}) 
-        error("Argument %d is not a string", ii); %$2 comment
-	end %$# comment
-end %!@#$%^&*()
+        error("Argument %d is not a string", ii); %$2 comment after code
+	end %$# comment after code
+end %!@#$%^&*()/-+,.[];\<>:{} comment after code
 
 %{
-	ml-1
-	multiline comment
+	grouped comments
+	deleting this comment will include deleting all nested comment groups in this scope
 	
 	%{
 		ml-2
-		multiline comment - level 2
-		
+		marked grouped comments - level 2
+
 		%{
 			ml-3
-			multiline comment - level 3
+			marked grouped comments - level 3
 		%}
 	%}
 %}
-str1 = which("stripmatlabcomments.py""); %$% this line contains an unterminated string
-str2 = sprintf('python "%s" -i "%s" -o "%s"', scriptPath, inputFile, outputFile); %q test quotes
-str3 = sprintf('%s -m "%s"', str1, str2); %q test quotes
-str4 = sprintf('%s -m ''%s''', str1, str2); %q test quotes
+str1 = which("stripmatlabcomments.py""); %$% this line contains an unterminated string - comment won't be deleted
+str2 = sprintf('python "%s" -i "%s" -o "%s"', scriptPath, inputFile, outputFile); %q comment after code
+str3 = sprintf('%s -m "%s"', str1, str2); %q comment after code
+str4 = sprintf('%s -m ''%s''', str1, str2); %q comment after code
 
 %{
 	!!!
 	all lines of an unterminated grouped comment will be deleted
-	!!!
+    
 	command = sprintf("%s '%s' -i '%s' -o '%s'", python, scriptPath,...
         inputFile, outputFile);
     if ~isempty(deletionMark)
--- tests/testingScript.m
+++ tests/testingScript.m
@@ -1,6 +1,13 @@
 clear;clc;
-testingMarks = ["$"; "$1"; "$2"; "$#"; "!@#$%^&*()"; "ml-1"; "ml-2";...
-    "ml-3"; "$%"; "q"; "!!!"];
+testingMarks = ["$"; "$1"; "$2"; "$#";...
+    "!@#$%^&*()/-+,.[];\<>:{}"; "ml-2"; "ml-3"; "$%"; "q"; "!!!"];
+environment = ver;
+isOctave = exist('OCTAVE_VERSION','builtin');
+% Octave needs backslash to be escaped
+if isOctave
+    testingMarks = ["$"; "$1"; "$2"; "$#";...
+      "!@#$%^&*()/-+,.[];\\<>:{}"; "ml-2"; "ml-3"; "$%"; "q"; "!!!"];
+end
 location = what('tests');
 path = location.path;
 
@@ -8,8 +15,9 @@ path = location.path;
 stripfile(sprintf("%s/%s", path, "testfile.m"), ...
         sprintf("%s/out-deleted-all.m", path));
 
+[len, ~] = size(testingMarks);
 % use deletion marks
-for ii = 1:length(testingMarks)
+for ii = 1:len
     stripfile(sprintf("%s/%s", path, "testfile.m"), ...
         sprintf("%s/out-%s.m", path, num2str(ii)),...
         strtrim(testingMarks(ii,:)));
